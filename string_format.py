e359957b32a69ad93fe6d74a1caf639ca064e0d8
datta kakde <85458867+datta184@users.noreply.github.com>
2021-12-11 15:53:27 +0530

    String formatting

---
+++ string_format.py
@@ -0,0 +1,13 @@
+print("*******************************************************************************")
+a=100
+print("the value of a is:{}".format(a))
+print("the value of a is:{:.2f}".format(a)) #here it print 2 decimal after value because :.2f
+print("*******************************************************************************")
+
+# string formating is useful in below case just see example
+
+frut_name = "orange"
+items = 12
+price = 9
+
+print("i want {} {} at price of {}".format(items,frut_name,price))
