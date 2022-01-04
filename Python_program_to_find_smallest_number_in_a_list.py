1d4359d893b6ae70460bb16d9dd18acdece0f0d6
datta kakde <85458867+datta184@users.noreply.github.com>
2022-01-05 00:58:05 +0530

    small number in the list

---
+++ Python_program_to_find_smallest_number_in_a_list.py
@@ -0,0 +1,8 @@
+# Python program to find smallest number in a list
+ 
+# list of numbers
+list1 = [23,56,11,22,78,99]
+ 
+ 
+# printing the minimum element
+print("Smallest element is:", min(list1))
