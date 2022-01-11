e31488af693acac8ec7ff0a856c36645deef830c
datta kakde <85458867+datta184@users.noreply.github.com>
2022-01-11 23:53:23 +0530

    printing key value
    {'john': 99, 'peter': 88, 'alex': 11, 'ram': 98}
    
     john : 99
    
     peter : 88
    
     alex : 11
    
     ram : 98

---
+++ printing_the_keys_and_values_from_dictionary.py
@@ -0,0 +1,11 @@
+# printing the keys and values from dictionary
+dic={
+    "john":99,
+    "peter":88,
+    "alex":11,
+    "ram":98
+}
+print(dic)
+
+for i ,j in dic.items():
+    print("\n",i,":",j)
