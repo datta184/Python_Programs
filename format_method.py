7c1cb902d9b4372c33ff55552ffe33a7c52a04b4
datta kakde <85458867+datta184@users.noreply.github.com>
2022-01-14 19:35:00 +0530

    format method

---
+++ format_method.py
@@ -0,0 +1,7 @@
+# format method 
+
+# using curly braces
+print("{} and {} are best friends".format("tom","jerry"))
+
+# using keyword argument
+print("{b} and {a} are best friends".format(a="tom",b="jerry"))
